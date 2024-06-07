c6632ac5d177a7045bcbfca6f7f1fdae8a273304
Cristopher Urbina H. <108028666+CrisU8@users.noreply.github.com>
2024-06-07 04:47:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,70 @@
 // create web server
-var express = require('express');
-var app = express();
-// create server
-var http = require('http').Server(app);
-// create socket
-var io = require('socket.io')(http);
+const http = require('http')
+const fs = require('fs')
 
-// when user connect
-io.on('connection', function(socket){
-  console.log('a user connected');
-  // when user disconnect
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  // when user send message
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+const server = http.createServer((req, res) => {
+    const { url, method } = req
 
-// create route
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+    if (url === '/' && method === 'GET') {
+        fs.readFile('./index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('500 - Internal Server Error')
+                return
+            }
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html')
+            res.end(data)
+        })
+    } else if (url === '/comments' && method === 'GET') {
+        fs.readFile('./comments.json', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('500 - Internal Server Error')
+                return
+            }
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.end(data)
+        })
+    } else if (url === '/comments' && method === 'POST') {
+        let body = []
 
-// run server
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        }).on('end', () => {
+            body = Buffer.concat(body).toString()
+            fs.readFile('./comments.json', (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' })
+                    res.end('500 - Internal Server Error')
+                    return
+                }
+                const comments = JSON.parse(data)
+                comments.push(JSON.parse(body))
+                fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' })
+                        res.end('500 - Internal Server Error')
+                        return
+                    }
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.end(JSON.stringify(comments))
+                })
+            })
+        })
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('404 - Not Found')
+    }
+})
+
+server.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000/')
+})
+
+// Path: index.html
+<!DOCTYPE html>
+<html lang="en">
